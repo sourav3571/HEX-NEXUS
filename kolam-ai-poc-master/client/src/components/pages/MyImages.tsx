5c2f171b1eb36ea5a7a11cee93c2e41bed1da638
hwhwnwn <souravbehera1adg@gmail.com>
2025-09-25 15:53:10 +0530

    added

--- kolam-ai-poc-master/client/src/components/pages/MyImages.tsx
+++
@@ -1,5 +0,0 @@
-export default function MyImages() {
-  return (
-    <><h1>My Images</h1></>
-  )
-}
--- kolam-ai-poc-master/server/install.sh
+++
@@ -1,5 +0,0 @@
-echo "Setting up Python virtual environment..."
-python3 -m venv venv
-source venv/bin/activate
-echo "Installing required Python packages..."
-pip install -r requirements.txt
